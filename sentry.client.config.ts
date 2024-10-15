e722352b806fba966922bf615ecee22d33dd4f28
hanbin <rj2303@naver.com>
2024-10-15 09:37:52 +0900

    install cypress

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,24 +5,23 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://2939b7c6d8d274b3a7d0454408c6fb9b@o4508120090935296.ingest.us.sentry.io/4508120106467328",
+	dsn: "https://2939b7c6d8d274b3a7d0454408c6fb9b@o4508120090935296.ingest.us.sentry.io/4508120106467328",
+	ignoreErrors: ["Something went wrong"],
 
-  // Add optional integrations for additional features
-  integrations: [
-    Sentry.replayIntegration(),
-  ],
+	// Add optional integrations for additional features
+	integrations: [Sentry.replayIntegration()],
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+	// Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+	tracesSampleRate: 1,
 
-  // Define how likely Replay events are sampled.
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+	// Define how likely Replay events are sampled.
+	// This sets the sample rate to be 10%. You may want this to be 100% while
+	// in development and sample at a lower rate in production
+	replaysSessionSampleRate: 0.1,
 
-  // Define how likely Replay events are sampled when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
+	// Define how likely Replay events are sampled when an error occurs.
+	replaysOnErrorSampleRate: 1.0,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+	// Setting this option to true will print useful information to the console while you're setting up Sentry.
+	debug: false,
 });
